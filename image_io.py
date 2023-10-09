b599e33b98da3d18b4449d70d05bc76a4c012381
cjt76 <cjt76@cornell.edu>
2023-10-09 13:41:48 -0400

    added starter code and separated files based on functionality

---
+++ image_io.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+class ImageIO:
+
+    def read_image(path):
+        # Read the image
+        pass
+    
+
+    def display_image(image, title):
+        # Display the image
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from image_io import ImageIO
+
+def main():
+    image = ImageIO.read_image("path_to_image.jpg")
+    # rest of main
+
+if __name__ == "__main__":
+    pass
+
+
