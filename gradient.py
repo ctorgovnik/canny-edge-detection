fixed some errors in gradient calculation and implemented non-maximum suppression (step 4)

gradient.py
@@ -3,25 +3,26 @@ import numpy as np
 class SobelOperator:
 
     def __init__(self):
-        Kx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
-        Ky = Kx.T
-        Ix = []
-        Iy = []
+        self.Kx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+        self.Ky = self.Kx.T
+        self.Ix = []
+        self.Iy = []
 
     def gradient_magnitude(self, im):
         self.Ix = self.convolve_2d(im, self.Kx)
         self.Iy = self.convolve_2d(im, self.Ky)
         magnitude = np.sqrt(np.square(self.Ix) + np.square(self.Iy))
+        magnitude = magnitude / magnitude.max() * 255
         return magnitude
 
     def gradient_direction(self):
-        theta = np.arctan2(self.Ix, self.Iy)
+        theta = np.arctan2(self.Iy, self.Ix)
         return theta
 
 
-    def convolve_2d(im, k):
+    def convolve_2d(self, im, k):
         Im, In = im.shape
-        Km, Kn = im.shape
+        Km, Kn = k.shape
 
         # add padding
         pad = (Kn - 1) // 2
@@ -29,12 +30,12 @@ class SobelOperator:
 
         output = np.zeros((Im, In))
 
-        for y in range (Km):
-            for x in range (Kn):
+        for y in range (Im):
+            for x in range (In):
                 # extract sub array the size of kernel
                 region = padded_img[y:y+Km, x:x+Kn]
 
                 output[y, x] = np.sum(region * k)
 
-            return output
+        return output
         

suppression.py
@@ -1,5 +1,59 @@
+import numpy as np
 
 class NonMaximumSupression:
 
-    def suppress(im, gradient_dir):
-        pass
+    @staticmethod
+    def suppress(direction, magnitude):
+        
+        M,N = magnitude.shape
+        suppressed_im = np.zeros((M, N))
+        neighbor1 = 0
+        neighbor2 = 0
+        for y in range(1, M-1):
+            for x in range(1, N-1):
+                dir = NonMaximumSupression.get_current_dir(direction[y, x])
+
+                if (dir == "horizontal"):
+                    neighbor1 = magnitude[y, x+1]
+                    neighbor2 = magnitude[y, x-1]
+
+                elif (dir == "vertical"):
+                    neighbor1 = magnitude[y+1, x]
+                    neighbor2 = magnitude[y-1, x]
+                
+                elif (dir == "positive_diagonal"):
+                    neighbor1 = magnitude[y-1, x+1]
+                    neighbor2 = magnitude[y+1, x-1]
+
+                elif (dir == "negative_diagonal"):
+                    neighbor1 = magnitude[y-1, x-1]
+                    neighbor2 = magnitude[y+1, x+1]
+
+
+                if (magnitude[y,x] >= neighbor1 and magnitude[y,x] >= neighbor2):
+
+                    suppressed_im[y,x] = magnitude[y, x]
+  
+                else:
+                    suppressed_im[y,x] = 0
+       
+        
+        return suppressed_im
+
+
+
+    @staticmethod
+    def get_current_dir(dir):
+        
+        angle = (dir * 180 / np.pi) % 360
+
+        if (angle >= 0 and angle < 22.5) or (angle >= 157.5 and angle < 202.5) or (angle >= 337.5 and angle <= 360):
+            return "horizontal"
+        elif (angle >= 22.5 and angle < 67.5) or (angle >= 202.5 and angle < 247.5):
+            return "positive_diagonal"
+        elif (angle >= 67.5 and angle < 112.5) or (angle >= 247.5 and angle < 292.5):
+            return "vertical"
+        elif (angle >= 112.5 and angle < 157.5) or (angle >= 292.5 and angle < 337.5):
+            return "negative_diagonal"
+        
+        return angle