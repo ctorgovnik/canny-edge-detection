867d3006553f22ca0afae9a85f31f0ce3be7b1e1
cjt76 <cjt76@cornell.edu>
2023-10-11 07:56:49 -0400

    implemented gradient magnitude and direction calculation (step 3)

--- gradient.py
+++ gradient.py
@@ -1,8 +1,40 @@
+import numpy as np
 
 class SobelOperator:
 
-    def gradient_magnitude(im):
-        pass
+    def __init__(self):
+        Kx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+        Ky = Kx.T
+        Ix = []
+        Iy = []
 
-    def gradient_direction(im):
-        pass
+    def gradient_magnitude(self, im):
+        self.Ix = self.convolve_2d(im, self.Kx)
+        self.Iy = self.convolve_2d(im, self.Ky)
+        magnitude = np.sqrt(np.square(self.Ix) + np.square(self.Iy))
+        return magnitude
+
+    def gradient_direction(self):
+        theta = np.arctan2(self.Ix, self.Iy)
+        return theta
+
+
+    def convolve_2d(im, k):
+        Im, In = im.shape
+        Km, Kn = im.shape
+
+        # add padding
+        pad = (Kn - 1) // 2
+        padded_img = np.pad(im, ((pad, pad), (pad, pad)), mode = 'constant')
+
+        output = np.zeros((Im, In))
+
+        for y in range (Km):
+            for x in range (Kn):
+                # extract sub array the size of kernel
+                region = padded_img[y:y+Km, x:x+Kn]
+
+                output[y, x] = np.sum(region * k)
+
+            return output
+        
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from image_io import ImageIO
 import smoothing
+from gradient import SobelOperator
 
 
 def main():
     color_im = ImageIO.read_image("goat.jpeg")
     gray_im = ImageIO.convert_grayscale(color_im)
     blur_im = smoothing.gaussian_blur(gray_im, smoothing.kernel_3)
+    
+    sobel_operator = SobelOperator()
+    magnitude = sobel_operator.gradient_magnitude(blur_im)
+    dir = sobel_operator.gradient_direction()
 
     ImageIO.display_image(color_im, None)
     ImageIO.display_image(gray_im, "gray")
