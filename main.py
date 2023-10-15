64e54a08066b6f9096289d99a7df770b759d0479
anya-yerramilli <anya.yerramilli@gmail.com>
2023-10-15 01:56:29 -0400

    Fixed thresholding/hysteresis bug (returning black image)

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from suppression import NonMaximumSupression
 from thresholding import DoubleThreshold
 from hysteresis import Hysteresis
 
-# import cv2
+import numpy as np
 
 
 def main():
-    color_im = ImageIO.read_image("emma_stone.jpg")
+    color_im = ImageIO.read_image("taylor_swift.webp")
     gray_im = ImageIO.convert_grayscale(color_im)
     blur_im = Smoothing.gaussian_blur(gray_im, Smoothing.kernel_3)
 
@@ -19,16 +19,16 @@ def main():
 
     suppressed_im = NonMaximumSupression.suppress(dir, magnitude)
 
-    thresh_im = DoubleThreshold.apply_threshold(suppressed_im, 0.05, 0.25)
+    thresh_im = DoubleThreshold.apply_threshold(suppressed_im, 0.1, 0.25)
 
     final_im = Hysteresis.apply_hysteresis(thresh_im)
 
-    ImageIO.display_image(color_im, None)
-    ImageIO.display_image(gray_im, "gray")
-    ImageIO.display_image(blur_im, "gray")
-    ImageIO.display_image(suppressed_im, "gray")
-    ImageIO.display_image(thresh_im, "gray")
-    ImageIO.display_image(final_im, "gray")
+    ImageIO.display_image(color_im, None, "Original Image")
+    # ImageIO.display_image(gray_im, "gray", "Gray Scale Image")
+    # ImageIO.display_image(blur_im, "gray", "Blurred Image")
+    # ImageIO.display_image(suppressed_im, "gray", "Supressed Image")
+    # ImageIO.display_image(thresh_im, "gray", "Thresholded Image")
+    ImageIO.display_image(final_im, "gray", "Final Image")
 
 
 if __name__ == "__main__":
